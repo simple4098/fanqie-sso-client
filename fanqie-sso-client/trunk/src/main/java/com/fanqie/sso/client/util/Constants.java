3ada5a9af3f19008996fd185a3f19540bb7fd0dc
zhanglin <zhanglin@671eefe9-2466-459f-b5fc-367f35298b91>
2015-04-06 09:34:18 +0000

    封装session 存储参数

---
+++ fanqie-sso-client/trunk/src/main/java/com/fanqie/sso/client/util/Constants.java
@@ -0,0 +1,12 @@
+package com.fanqie.sso.client.util;
+
+/**
+ *
+ * @author 番茄木-张林
+ * @date 2015-04-06
+ * sso-client v1.0
+ */
+public class Constants {
+    //session 存用户参数名称
+    public static final String CONST_CAS_ASSERTION = "_const_cas_assertion_";
+}
